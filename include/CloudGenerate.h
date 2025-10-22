2417a374339a38276fa0c75ad10aba5d083c06a3
Gloomyer <419209765@qq.com>
2025-10-22 11:27:08 +0800

    Add files via upload

--- include/CloudGenerate.h
+++ include/CloudGenerate.h
@@ -2,19 +2,28 @@
 #include <vector>
 #include <string>
 #include <fstream>
-
-// OpenCV 相关头文件
 #include <opencv2/core.hpp>
 #include <opencv2/calib3d.hpp> 
-#include <opencv2/sfm.hpp>
-
-// 对于 Point3d 和 Point2d 类型
 #include <opencv2/core/types.hpp>
 
 
-bool PnP(std::vector<cv::Point3d>& model_points, std::vector<cv::Point2d>& image_points, cv::Mat& camera_matrix, cv::Mat& diffcoeffs, cv::Mat& translate_vec, cv::Mat& rotate_vec);
+bool PnP(
+    std::vector<cv::Point3d>& model_points, 
+    std::vector<cv::Point2d>& image_points, 
+    cv::Mat& camera_matrix, 
+    cv::Mat& diffcoeffs, 
+    cv::Mat& translate_vec, 
+    cv::Mat& rotate_vec
+);
 
-bool Bundle_Adjustment(const std::vector<cv::Mat>& points2d_all, const std::vector<cv::Mat>& points3d_all, const std::vector<cv::Mat>& K, const std::vector<cv::Mat>& Rs, const std::vector<cv::Mat>& ts);
+bool SimpleBundleAdjustAfterPnP(
+    std::vector<cv::Point3d>& points3D,
+    const std::vector<std::vector<cv::Point2d>>& projections2D_all,
+    const std::vector<cv::Mat>& Ks,
+    std::vector<cv::Mat>& Rs,
+    std::vector<cv::Mat>& ts,
+    int iterations = 5
+);
 
 cv::Vec4d rotationMatrixToQuaternion(const cv::Mat& R);
 
@@ -29,4 +38,18 @@ void Export_To_NVM(
     const std::vector<cv::Mat>& images
 );
 
-bool IncreaseCloud(std::vector<std::vector<int>>& point3DIds,std::vector<std::vector<cv::Point2d>>& feature_points,int camera_number,int num, cv::Mat& old_camera, std::vector<cv::Point2d>& old_image_points, std::vector<cv::Point3d>& model_points, std::vector<cv::Point2d>& image_points, cv::Mat& camera_matrix, cv::Mat& diffcoeffs, cv::Mat& translate_vec, cv::Mat& rotate_vec, std::vector<std::vector<int>>& viewIndices);
+bool IncreaseCloud(
+    std::vector<std::vector<int>>& point3DIds,
+    std::vector<std::vector<cv::Point2d>>& feature_points,
+    int camera_number,
+    int num, 
+    cv::Mat& old_camera, 
+    std::vector<cv::Point2d>& old_image_points, 
+    std::vector<cv::Point3d>& model_points, 
+    std::vector<cv::Point2d>& image_points, 
+    cv::Mat& camera_matrix, 
+    cv::Mat& diffcoeffs, 
+    cv::Mat& translate_vec, 
+    cv::Mat& rotate_vec, 
+    std::vector<std::vector<int>>& viewIndices
+);
